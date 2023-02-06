a5081afbf225228ba728abfa9ef9b501acfa9556
Tini <94822768+Tinichiou@users.noreply.github.com>
2023-02-06 22:31:38 +0800

    Update installextra.R

--- installextra.R
+++ installextra.R
@@ -1,2 +1,3 @@
-download.file("https://cran.r-project.org/src/contrib/Archive/extraTrees/extraTrees_1.0.5.tar.gz", "extraTrees_1.0.5.tar.gz")
-install.packages("extraTrees_1.0.5.tar.gz", repos = NULL, type="source")
+#download.file("https://cran.r-project.org/src/contrib/Archive/extraTrees/extraTrees_1.0.5.tar.gz", "extraTrees_1.0.5.tar.gz")
+#install.packages("extraTrees_1.0.5.tar.gz", repos = NULL, type="source")
+devtools::install_version('extraTrees', version = '1.0.5')
