27e174a7cd9805ca99c09130a90af0e5f18c3162
Tini <94822768+Tinichiou@users.noreply.github.com>
2023-02-06 22:19:31 +0800

    Create installextra.R

---
+++ installextra.R
@@ -0,0 +1,2 @@
+download.file("https://cran.r-project.org/src/contrib/Archive/extraTrees/extraTrees_1.0.5.tar.gz", "extraTrees_1.0.5.tar.gz")
+install.packages("extraTrees_1.0.5.tar.gz", repos = NULL, type="source")
